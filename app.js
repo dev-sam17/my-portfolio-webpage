05816fe6bc3bcb5d9d77cf462816467de786a32d
Saurav Kumar <dev_sam17@protonmail.com>
2023-09-21 12:02:13 +0530

    updated interests section

--- app.js
+++ app.js
@@ -10,4 +10,16 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+	const error = new Error('Not found')
+	error.status(404)
+	next(error)
+})
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500)
+	res.redirect('/notfound.html')
+	
+})
+
 module.exports = app;
